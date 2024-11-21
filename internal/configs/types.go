e616c77438e926a1a9634bd1f7b7b38f865d6a1a
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-11-21 16:10:44 +0700

    feat(config): add database configuration with data source name

--- internal/configs/types.go
+++ internal/configs/types.go
@@ -2,10 +2,15 @@ package configs
 
 type (
 	Config struct {
-		Service Service `mapstructure:"service"`
+		Service  Service  `mapstructure:"service"`
+		Database Database `mapstructure:"database"`
 	}
 
 	Service struct {
 		Port string `mapstructure:"port"`
+	}
+
+	Database struct {
+		DataSourceName string `mapstructure:"dataSourceName"`
 	}
 )
