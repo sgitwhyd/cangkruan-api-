0047b1e12754a7c78b11b50a8137c8f5cf4522ce
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-11-22 13:53:49 +0700

    feat(jwt): add JWT token creation and validation functions; update config for secretJWT

--- internal/configs/types.go
+++ internal/configs/types.go
@@ -7,7 +7,8 @@ type (
 	}
 
 	Service struct {
-		Port string `mapstructure:"port"`
+		Port      string `mapstructure:"port"`
+		SecretJWT string `mapstructure:"secretJWT"`
 	}
 
 	Database struct {
